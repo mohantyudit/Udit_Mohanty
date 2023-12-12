6604fd0c8167513e75823980af2129fbf2abd022
mohantyudit <108442058+mohantyudit@users.noreply.github.com>
2023-12-12 00:50:43 -0800

    Code Structure

--- udit_mohanty/contacts/models.py
+++ udit_mohanty/contacts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    email = models.EmailField()
+    notes = models.TextField(max_length=200, default="")
+    time = models.TimeField(auto_now=False, auto_now_add=False)
--- udit_mohanty/contacts/views.py
+++ udit_mohanty/contacts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Contact
 
 # Create your views here.
+class ContactListView(ListView):
+    model = Contact
